ac25e6e55829d61cde9e7d732f9e510bb22ea6e6
lucafmarques <lucafmarqs@gmail.com>
2023-08-31 02:56:14 -0300

    fix: Stop error from turning into constant

--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -6,17 +6,17 @@ var t = true
 
 type Receiver[T any] struct {
 	Queue chan *T
-	err   error
 }
 
 func (r *Receiver[T]) Receive(item *T, ok *bool) error {
+	var err error
 	defer func() {
 		recover()
-		r.err = errors.New("queue is closed")
+		err = errors.New("queue is closed")
 	}()
 
 	r.Queue <- item
 	ok = &t
 
-	return r.err
+	return err
 }
