c63ad6d1143bf6dba9a5bc96e15ae72824a5d751
lucafmarques <lucafmarqs@gmail.com>
2023-08-31 02:56:14 -0300

    chore: Add recover to internal receiver

--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -1,13 +1,22 @@
 package receiver
 
+import "errors"
+
 var t = true
 
 type Receiver[T any] struct {
 	Queue chan *T
+	err   error
 }
 
 func (r *Receiver[T]) Receive(item *T, ok *bool) error {
+	defer func() {
+		recover()
+		r.err = errors.New("queue is closed")
+	}()
+
 	r.Queue <- item
 	ok = &t
-	return nil
+
+	return r.err
 }
