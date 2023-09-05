39c34cb53acdf516614fe7bca70fe87bcc413865
lucafmarques <lucafmarqs@gmail.com>
2023-09-05 00:03:52 -0300

    refactor: Simplify internal type Receive

--- internal/receiver.go
+++ internal/receiver.go
@@ -4,7 +4,7 @@ import "errors"
 
 // Receiver needs to be exported to be used as an rpc handler.
 type Receiver[T any] struct {
-	channel chan *T
+	Channel chan *T
 }
 
 func NewReceiver[T any](buf uint) *Receiver[T] { return &Receiver[T]{make(chan *T, buf)} }
@@ -18,10 +18,7 @@ func (r *Receiver[T]) Send(item *T, ok *bool) error {
 		err = errors.New("queue is closed")
 	}()
 
-	r.channel <- item
+	r.Channel <- item
 
 	return err
 }
-
-func (r Receiver[T]) Close()           { close(r.channel) }
-func (r Receiver[T]) Channel() chan *T { return r.channel }
